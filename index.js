c11e4d37833426fca0379b37255d2b32776913f3
unknown <jandyroachy@gmail.com>
2025-01-31 17:24:09 +0530

    done

--- index.js
+++ index.js
@@ -34,18 +34,32 @@
 
 const express = require('express');
 const { resolve } = require('path');
+const bodyParser = require ("body-parser")
+
 
 const app = express();
 const port = 3010;
+app.use(bodyParser.json())
+const students = require("./data.json")
 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post('/students/above-threshold', (req, res) => {
+  const { threshold } = req.body;
+
+  if (typeof threshold !== 'number' || isNaN(threshold)) {
+    return res.json({count:0,
+      students: [ ] });
+  }
+
+  const filteredStudents = students.filter(student => student.total > threshold);
+
+   res.json({
+    count: filteredStudents.length,
+    students: filteredStudents.map(student => ({ name: student.name, total: student.total }))
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
